2191a26f101449c605f06c8debefc88878d15d45
Josiah Boning <jboning@gmail.com>
2014-02-17 14:56:37 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='dogma',
+      version='0.1',
+      description='Python bindings for libdogma',
+      author='Josiah Boning',
+      author_email='jboning@gmail.com',
+      py_modules=['dogma']
+     )
